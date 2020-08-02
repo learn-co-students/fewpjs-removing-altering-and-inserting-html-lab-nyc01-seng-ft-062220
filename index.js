fb718941bf375a502d8ed566be987c8bf8d9133a
roman9891 <roman9891@gmail.com>
2020-08-02 16:24:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const element = document.querySelector('main')
+element.remove()
+const newHeader = document.createElement('h1')
+newHeader.innerHTML = ('victory')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML += 'Roman is the champion'
+console.log("log \/")
+//console.log(element)
