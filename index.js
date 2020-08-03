a52c9aadc7b84659e7c1213ef23f3fd432ae6248
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-03 01:27:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
 document.querySelector("main").remove();
 
-let newHeader = document.createElement("h1#victory");
+let newHeader = document.createElement("h1");
+newHeader.id = "id";
 newHeader.classname = "Victory";
